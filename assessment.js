713782c94dc04b6f55ed21a70dd03c2998285e25
hariyanto1990 <hariyanto.ambon1990@gmail.com>
2021-06-17 18:06:36 +0900

    assessment 1 & 2

---
+++ assessment.js
@@ -0,0 +1,14 @@
+
+// 1. Print range 1 sampai dengan 10 
+for(let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// 2. Pengecekan Nama Perusahaan
+let check = 'Compnet';
+    if(check == 'Compnet') {
+    console.log('Nama Perusahaan Kita');
+    } else {
+    console.log('Bukan Nama Perusahaan Kita');
+    }
+
